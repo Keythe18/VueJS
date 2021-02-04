adf314cd2b65524c0bd5c9aed18e4b10f64256e5
Keythe18 <alexis.lansucki@gmail.com>
2021-02-04 15:40:20 +0100

    VueJS

--- 4 - NodeJS/Intro NPM/my-project/src/App.vue
+++ 4 - NodeJS/Intro NPM/my-project/src/App.vue
@@ -1,12 +1,65 @@
 <template>
-  <div></div>
+  <button v-on:click="hideBtn">Btn</button>
+  <button v-on:click="resetBtn">Reset</button>
+  <div>
+    <Personnes
+      v-for="personne in personnes"
+      v-bind:key="personne.id"
+      v-bind:prenom="personne.prenom"
+      v-bind:nom="personne.nom"
+      v-bind:age="personne.age"
+      v-bind:majeur="personne.majeur"
+    ></Personnes>
+  </div>
 </template>
 
 <script>
+import Personnes from "./components/Personnes";
+
 export default {
   name: "App",
-  components: {},
+
+  data() {
+    return {
+      personnes: [
+        { id: 0, prenom: "Tom", nom: "Hanks", age: 12, majeur: true },
+        { id: 1, prenom: "Tom", nom: "Hanks", age: 15, majeur: true },
+        { id: 2, prenom: "Tom", nom: "Hanks", age: 18, majeur: true },
+        { id: 3, prenom: "Tom", nom: "Hanks", age: 20, majeur: true },
+        { id: 4, prenom: "Tom", nom: "Hanks", age: 16, majeur: true },
+        { id: 5, prenom: "Tom", nom: "Hanks", age: 13, majeur: true },
+        { id: 6, prenom: "Tom", nom: "Hanks", age: 24, majeur: true },
+        { id: 7, prenom: "Tom", nom: "Hanks", age: 39, majeur: true },
+        { id: 8, prenom: "Tom", nom: "Hanks", age: 39, majeur: true },
+        { id: 9, prenom: "Tom", nom: "Hanks", age: 39, majeur: true },
+        { id: 10, prenom: "Tom", nom: "Hanks", age: 39, majeur: true },
+        { id: 11, prenom: "Tom", nom: "Hanks", age: 39, majeur: true },
+      ],
+    };
+  },
+
+  components: {
+    Personnes,
+  },
+
+  methods: {
+    hideBtn() {
+      this.personnes.forEach((personne) => {
+        personne.majeur = personne.age < 18 ? false : true;
+      });
+    },
+    resetBtn() {
+      this.personnes.forEach((personne) => {
+        personne.majeur = true;
+      });
+    },
+  },
 };
 </script>
 
-<style></style>
+<style>
+button {
+  padding: 5px;
+  font-size: 15px;
+}
+</style>
---
+++ 4 - NodeJS/Intro NPM/my-project/src/components/Personnes.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="majeur">
+    <h1>{{ prenom }} {{ nom }}</h1>
+    <p>{{ age }} Ans</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Personnes",
+  props: {
+    id: Number,
+    nom: String,
+    prenom: String,
+    age: Number,
+    majeur: Boolean,
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  font-family: sans-serif;
+  color: blue;
+}
+p {
+  font-family: sans-serif;
+  color: grey;
+}
+div {
+  font-family: sans-serif;
+  border: 2px solid gray;
+  width: 35%;
+  margin: 5px 5px;
+  border-radius: 5px;
+  float: left;
+  text-align: center;
+}
+</style>
