fa3aa495dae11ad12be7becf95d6c0f65789fc62
Keythe18 <alexis.lansucki@gmail.com>
2021-02-04 13:52:29 +0100

    VueJS

---
+++ 4 - NodeJS/Intro NPM/my-project/src/AppBackup.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <TitreBleu
+      v-for="titre in titres"
+      v-bind:key="titre.id"
+      v-bind:affichageEtat="affiche"
+      v-bind:titre="titre.titre"
+    ></TitreBleu>
+
+    <button v-on:click="toogle">Cacher</button>
+  </div>
+</template>
+
+<script>
+import TitreBleu from "./components/TitreBleu";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      affiche: true,
+      titres: [
+        { id: 0, titre: "Bonjour" },
+        { id: 1, titre: "Bonsoir" },
+      ],
+    };
+  },
+  methods: {
+    toogle() {
+      this.affiche = this.affiche ? false : true;
+    },
+  },
+  components: {
+    TitreBleu,
+  },
+};
+</script>
+
+<style></style>
---
+++ 4 - NodeJS/Intro NPM/my-project/src/components/TitreBleu.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 v-if="affichageEtat">{{ titre }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TitreBleu",
+  props: {
+    titre: String,
+    affichageEtat: Boolean,
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  font-family: sans-serif;
+  color: blue;
+}
+</style>
